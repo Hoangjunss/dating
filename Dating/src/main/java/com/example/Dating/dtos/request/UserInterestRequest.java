7acf63db110a265bd1eb4b160615aad2f2a4945f
TDKhoa2712 <khoatd2712@gmail.com>
2026-03-05 04:01:09 +0700

    feat(UserInterest): expand RequestDTO, service and Controller
     - Add Validation to request DTOs
    - Adjust ServiceImpl to handle CRUD logic
    - Add new endpoint, modify entity response in Controller

--- Dating/src/main/java/com/example/Dating/dtos/request/UserInterestRequest.java
+++ Dating/src/main/java/com/example/Dating/dtos/request/UserInterestRequest.java
@@ -1,11 +1,16 @@
 package com.example.Dating.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class UserInterestRequest {
+    
+    @NotNull(message = "User ID cannot be null")
     private UUID userId;
+    
+    @NotNull(message = "Interest ID cannot be null")
     private UUID interestId;
 }
