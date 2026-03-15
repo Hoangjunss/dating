b2887d2392e41a44c3744f0cb9c9d60a211ac6e0
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2026-03-15 22:35:30 +0700

    feat(profile): add user profile page

--- DatingFE/dating/src/App.jsx
+++ DatingFE/dating/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import SignUpPage from './page/SignupPage'
 import LoginPage from './page/LoginPage'
 import HomePage from './page/HomePage'
+import ProfilePage from './page/ProfilePage'
 import { Route, Routes } from 'react-router-dom'
 import { Toaster } from "react-hot-toast";
 
@@ -13,7 +14,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/signup' element={<SignUpPage/>} />
-         <Route path='/login' element={<LoginPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
       </Routes>
       <Toaster />
     </div>
