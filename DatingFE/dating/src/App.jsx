711e29b80861453490869add67fe88b48dbb423a
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2026-03-15 21:38:52 +0700

    feat(router): integrate react-router and add home route with navbar layout

---
+++ DatingFE/dating/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div >
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+      </Routes>
+    </div>
+    
+  )
+}
+
+export default App
